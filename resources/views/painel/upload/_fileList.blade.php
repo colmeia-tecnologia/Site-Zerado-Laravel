74ade7f04efd2e67fa43a7cee0641d055f0ee3ee
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2018-03-23 10:14:32 -0300

    Upload - Exibindo nome das imagens

--- resources/views/painel/upload/_fileList.blade.php
+++ resources/views/painel/upload/_fileList.blade.php
@@ -3,6 +3,10 @@
         <div class='upload-image'>
             <img src='{{asset($file)}}' class='img-responsive'>
         </div>
+
+        <div class='col-md-12 margin-top'>
+            {{str_replace('img/', '', $file)}}
+        </div>
         
         <div class='col-md-12 text-center margin-top-p'>
             <a class='btn btn-default uploadSelectImage' data-name='{{asset($file)}}'>
